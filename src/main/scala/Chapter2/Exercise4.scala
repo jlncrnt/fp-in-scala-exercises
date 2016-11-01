cc9c8fda766e49679f1055caecc383cb49e1d10c
jlncrnt <julien.cornut@hesge.ch>
2016-11-01 15:13:18 +0100

    Removed doc

---
+++ src/main/scala/Chapter2/Exercise4.scala
@@ -0,0 +1,19 @@
+package Chapter2
+
+/**
+  * Write the implementation of curry function
+  */
+object Exercise3 extends App {
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a: A, b: B) // f(A,B) returns C and => is right associative
+  }
+
+  // IO side effect as last call to prettify run
+  def madras: (List[Char], Int) => String = (a: List[Char], b: Int) => a.mkString + b.toString
+  def recipe = Exercise3 curry madras
+  def meal = recipe("Vindaloo".toList)
+  val mealNumber = meal(8)
+  println("Result of a curry function parametrized with Vindaloo and called with 8 : " + mealNumber)
+
+}
