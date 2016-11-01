46e5fd873fc1322701c31430c45a09a657cb032c
jlncrnt <julien.cornut@hesge.ch>
2016-11-01 13:36:51 +0100

    Change Test Exercises 1

--- src/test/scala/Chapter2/Exercise1Tests.scala
+++ src/test/scala/Chapter2/Exercise1Tests.scala
@@ -8,15 +8,15 @@ import org.scalatest.{FlatSpec, Matchers}
 class Exercise1Tests extends FlatSpec with Matchers {
 
   "1th Fibonacci number of Exercise 2.1" should "be 0" in {
-    Chapter2.Exercise1 fibonacci 1 shouldBe 0
+    Exercise1 fibonacci 1 shouldBe 0
   }
 
   "2th Fibonacci number of Exercise 2.1" should "be 1" in {
-    Chapter2.Exercise1 fibonacci 2 shouldBe 1
+    Exercise1 fibonacci 2 shouldBe 1
   }
 
   "8th Fibonacci number of Exercise 2.1" should "be 13" in {
-    Chapter2.Exercise1 fibonacci 8 shouldBe 13
+    Exercise1 fibonacci 8 shouldBe 13
   }
 
 }
