d9b19daf2c14118252adcc4910ae2d2e9e033a1a
jlncrnt <julien.cornut@hesge.ch>
2016-11-01 13:36:13 +0100

    Tests for exercise 2

---
+++ src/test/scala/Chapter2/Exercise2Tests.scala
@@ -0,0 +1,36 @@
+package Chapter2
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by julien on 01.11.16.
+  */
+class Exercise2Tests extends FlatSpec with Matchers {
+
+  "A sorted Array of int" should "return true with function <" in {
+    val arr: Array[Int] = Array[Int](1,2,3,4,5,6,7,8,9)
+    val fun: (Int, Int) => Boolean = (a: Int, b: Int) => a < b
+    assert(Exercise2.isSorted(arr, fun))
+  }
+
+  "An unsorted Array of int" should "return false with function <" in {
+    val arr: Array[Int] = Array[Int](1,3,2,4)
+    val fun: (Int, Int) => Boolean = (a: Int, b: Int) => a < b
+    assert(! Exercise2.isSorted(arr, fun) )
+  }
+
+  "A sorted Array of double" should "return true with function <" in {
+    val arr: Array[Double] = Array[Double](1,2,3,4,5,6,7,8,9)
+    def fun: (Double, Double) => Boolean = (a: Double, b: Double) => a < b
+    assert(Exercise2 isSorted(arr, fun))
+  }
+
+  "An array of arbitrary objects" should "return true with function <" in {
+    case class Arbitrary(n: Int) { def <(that: Arbitrary) = this.n < that.n }
+    def fun: (Arbitrary, Arbitrary) => Boolean = (a: Arbitrary, b:Arbitrary) => a < b
+    val (smaller, bigger) = (Arbitrary(0), Arbitrary(1))
+    val arr: Array[Arbitrary] = Array[Arbitrary](smaller, bigger)
+    assert(Exercise2 isSorted(arr, fun))
+  }
+
+}
