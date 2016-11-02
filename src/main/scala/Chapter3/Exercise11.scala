20f1d254bc3e40bca110f8eb777a4120c299a076
jlncrnt <julien.cornut@hesge.ch>
2016-11-02 23:28:38 +0100

    Added Chapter 3 Exercise 11

---
+++ src/main/scala/Chapter3/Exercise11.scala
@@ -0,0 +1,21 @@
+package Chapter3
+
+object Exercise11 extends App {
+
+  /**
+    * Write sum, product and length using foldLeft
+    */
+
+  import Exercise10.foldLeft
+
+  def sum       (as: List[Int]) : Int = foldLeft(as,0)(_ + _)
+  def product   (as: List[Int]) : Int = foldLeft(as,1)(_ * _)
+  def length[A] (as: List[A])   : Int = foldLeft(as,0)((x,_) => x + 1)
+
+  val list = List(1,2,3,4,5)
+
+  println("sum of List(1,2,3,4,5) is : " + sum(list))
+  println("product of List(1,2,3,4,5) is : " + product(list))
+  println("lenght of List(1,2,3,4,5) is : " + length(list))
+
+}
