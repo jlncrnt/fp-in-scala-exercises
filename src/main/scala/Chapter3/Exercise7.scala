b8af784c31ad5bcda98b8d23f634a52886226c6f
jlncrnt <julien.cornut@hesge.ch>
2016-11-02 22:32:52 +0100

    Added Chapter 3 Exercise 8

--- src/main/scala/Chapter3/Exercise7.scala
+++ src/main/scala/Chapter3/Exercise7.scala
@@ -2,12 +2,11 @@ package Chapter3
 
 object Exercise7 extends App {
 
-  /**
-    * In the implementation of product given in the book,
-    * foldRight is used as argument. For any function call
-    * by value (not by name), the argument are evaluated
-    * before the call. So the arguments will have to be
-    * evaluated until the end of list.
-    */
+  println()
+  println(""" | In the implementation of product given in the book,
+              | foldRight is used as argument. For any function call
+              | by value (not by name), the argument are evaluated
+              | before the call. So the arguments will have to be
+              | evaluated until the end of list.""".stripMargin)
 
 }
---
+++ src/main/scala/Chapter3/Exercise8.scala
@@ -0,0 +1,27 @@
+package Chapter3
+
+object Exercise8 extends App {
+
+  println()
+  println(""" | It will build a list, calling Cons on each element
+              | with the rest of the list as second argument.
+              | So the resulting list is structurally the same
+              | as the one passed in argument.
+              | """.stripMargin)
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A,B) => B) : B = as match {
+    case Nil => z
+    case Cons(x,xs) => f(x, foldRight(xs,z)(f))
+  }
+
+  val list = List(1,2,3)
+  val res = foldRight(list, Nil:List[Int])(Cons(_,_))
+
+  val isSame = list match {
+    case `res` => true
+    case _ => false
+  }
+
+  println("Folded-right list result structurally equals its argument : " + isSame)
+
+}
