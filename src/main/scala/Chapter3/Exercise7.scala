6c947ea0cb750eff2153d6cf5bc367fedd3c680d
jlncrnt <julien.cornut@hesge.ch>
2016-11-02 21:56:25 +0100

    Added Chapter 3 Exercise 7

--- src/main/scala/Chapter3/Exercise7.scala
+++ src/main/scala/Chapter3/Exercise7.scala
@@ -3,10 +3,11 @@ package Chapter3
 object Exercise7 extends App {
 
   /**
-    * Product cannot halts the recursion, since foldRight
-    * is not tail recursive. The call to itself is not the
-    * last operation of the recursion, it still have to multiply
-    * the return by the value in the outer recursion.
+    * In the implementation of product given in the book,
+    * foldRight is used as argument. For any function call
+    * by value (not by name), the argument are evaluated
+    * before the call. So the arguments will have to be
+    * evaluated until the end of list.
     */
 
 }
