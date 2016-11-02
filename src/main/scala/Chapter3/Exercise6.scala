313c26c4aaab782290daf3b0a7c764df265e050e
jlncrnt <julien.cornut@hesge.ch>
2016-11-02 21:48:08 +0100

    Added Chapter 3 Exercise 6

---
+++ src/main/scala/Chapter3/Exercise6.scala
@@ -0,0 +1,22 @@
+package Chapter3
+
+object Exercise6 extends App {
+
+  /**
+    * Implement function init
+    * "List" refers to the implementation in this package.
+    */
+
+  def init[T](as: List[T]): List[T] = as match {
+    case Nil => Nil
+    case Cons(x,Nil) => Nil
+    case Cons(x,xs) => Cons(x, init(xs))
+  }
+
+  val list = List[Int](1,2,3,4,5,6)
+
+  println("List : " + list)
+
+  println("Drop 2 : " + init(list))
+
+}
