70ab7da0f73042119e46f6e9f2a445b9d31e19c3
jlncrnt <julien.cornut@hesge.ch>
2016-11-03 11:41:12 +0100

    Added Chapter 3 Exercise 14

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Repo to keep (and share) my solutions to the Scala red book exercises
 
 Each exercise extends App so it can be run separately and has a result print as a side effect to make the run prettier. Use `run-main Chapter<N>.Exercise<N>`.
+ 
---
+++ src/main/scala/Chapter3/Exercise14.scala
@@ -0,0 +1,18 @@
+package Chapter3
+
+object Exercise14 extends App {
+
+  /**
+    * Implement append in terms of foldLeft or foldRight
+    */
+
+  import Exercise10.foldLeft
+  import Exercise8.foldRight
+
+  def append[T](l: List[T], e: List[T]): List[T] = foldRight(l, e)(Cons(_,_))
+
+  val list = List(1, 2, 3, 4, 5)
+
+  println("Append List(6,7,8) to right of List(1,2,3,4,5) gives : " + append(list,List(6,7,8)))
+
+}
